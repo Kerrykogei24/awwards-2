71c6d749b0df892575143036cff1f53bba2f27a8
Cynthia Kitili <nyambucindy0@gmail.com>
2021-01-23 11:40:07 +0300

    import models in admin

--- Awwards/settings.py
+++ Awwards/settings.py
@@ -35,10 +35,10 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'rate',
-    'bootstrap3',
+    'bootstrap4',
     'tinymce',
     'rest_framework',
-    'rest_framework.authtoken'
+    # 'rest_framework.authtoken'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
