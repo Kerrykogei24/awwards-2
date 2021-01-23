a57d8e04560b5e5c7ff1f07757f3e902eb9a47e7
Cynthia Kitili <nyambucindy0@gmail.com>
2021-01-23 11:50:01 +0300

    add home function

--- rate/views.py
+++ rate/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, redirect, get_object_or_404
+from django.http import HttpResponse, Http404,HttpResponseRedirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout as django_logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_protect
+from django.contrib import messages
+from .models import *
+from .forms import *
+from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import *
+# from rest_framework import viewsets
+# from rest_framework import permissions
+from rest_framework import status
+from .permissions import IsAdminOrReadOnly
+# from .token_generator import account_activation_token
 
-# Create your views here.
+
+def home(request):
+    projects = Project.get_projects()
+    context={
+        'projects' : projects,
+    }
+    return render(request,"home.html", context)
