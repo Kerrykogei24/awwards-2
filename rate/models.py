cb1821609ba912b542bbd4ae11023645d7cf3907
Cynthia Kitili <nyambucindy0@gmail.com>
2021-01-23 11:59:21 +0300

    commit profile model

--- rate/models.py
+++ rate/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
+import numpy as np
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.png', upload_to='profile_pics')
+    bio = models.TextField(max_length=100)
+    contact=models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save_profile(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+    
+    @classmethod
+    def get_profile(cls):
+        profile = Profile.objects.all()
+
+        return profile
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+class tags(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+    def save_tags(self):
+        self.save()
+
+    def delete_tags(self):
+        self.delete()
+
+class Location(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
