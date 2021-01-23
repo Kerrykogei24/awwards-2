fd231c3e0ae0e6f431b6703613b56379cc76ecf0
Cynthia Kitili <nyambucindy0@gmail.com>
2021-01-23 11:51:58 +0300

    add logout and new profile functions

--- rate/views.py
+++ rate/views.py
@@ -59,3 +59,33 @@ def profile(request,profile_id):
         'project':project
     }
     return render(request,"profile.html", context)    
+
+@login_required
+def logout(request):
+    django_logout(request)
+    return  HttpResponseRedirect('/')
+
+@login_required(login_url='/accounts/login/')
+def new_profile(request):
+    project = Project.objects.filter(author=request.user).order_by('-date_posted')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form,
+        'project' : project
+    }
+    return render(request, 'profile/profile.html', context)    
