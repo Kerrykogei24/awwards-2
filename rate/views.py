6f2fbc8cbabed7fbc11ab4b0a06c47529a21c043
Cynthia Kitili <nyambucindy0@gmail.com>
2021-01-23 11:51:08 +0300

    add project and profile functions

--- rate/views.py
+++ rate/views.py
@@ -24,4 +24,38 @@ def home(request):
     context={
         'projects' : projects,
     }
-    return render(request,"home.html", context)
+    return render(request,"index.html", context)
+
+@login_required(login_url='/accounts/login/')
+def project(request):
+    current_user = request.user
+    profiles = Profile.get_profile()
+    for profile in profiles:
+        if profile.user.id == current_user.id:
+            if request.method == 'POST':
+                form = ProjectForm(request.POST,request.FILES)
+                if form.is_valid():
+                    new_project = form.save(commit=False)
+                    new_project.author = current_user
+                    new_project.profile = profile
+                    new_project.save()
+                    return redirect('home')
+            else:
+                form = ProjectForm()
+                
+            context = {
+                'user':current_user,
+                'form':form
+            }
+            return render(request,'project.html', context)
+    
+@login_required(login_url='/accounts/login/')
+def profile(request,profile_id):
+    profile = Profile.objects.get(pk = profile_id)
+    project = Project.objects.filter(profile_id=profile).all()
+    
+    context = {
+        'profile':profile,
+        'project':project
+    }
+    return render(request,"profile.html", context)    
